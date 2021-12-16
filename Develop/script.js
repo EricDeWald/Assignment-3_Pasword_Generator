05d7647c0468ce35064c12a3a412cd7a6f42c4da
EricDeWald <eric.l.dewald@gmail.com>
2021-12-15 22:19:25 -0600

    close to the end

--- Develop/script.js
+++ Develop/script.js
@@ -1,19 +1,21 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
 var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
 var spclChar = [" ","'","!",'"',"#","$","%","&","(",")","*","+",",","-",".","/",":",";","<","=",">","?","@","]","^","_","`","{","|","}","~"]
-
 
 // Write password to the #password input
 function writePassword(){
   
   var pw_Length = prompt("How many long do you want your password?")
+  console.log(pw_Length)
+
   if (pw_Length <8 || pw_Length>128){
     alert("!!!MUST BE BETWEEN 8 AND 128!!!")
-  };
 
+    // console.log(pw_Length)
+  };
+  
   var password = generatePassword();
 
   function generatePassword(){
@@ -29,22 +31,20 @@ function writePassword(){
     };
     
     console.log(charList)
+    var pw_char = []
 
     for (var i = 0; i < pw_Length; i++) {
-      pw_char = []
       
-      charList[Math.floor(0,pw_Length)];
-    
-    
-      console.log(pw_char)
-    
-      return pw_char
+      pw_char = pw_char.concat(charList[Math.floor(Math.random() * pw_Length)]);
+       
     };
+
+    console.log(pw_char)
+    
+    return password
+    
   };
   
-  
-
-
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
